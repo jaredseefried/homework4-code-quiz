18e528baf86f94df260974019d31bdef27de0cf8
jaredcito <jared.seefried@yahoo.com>
2020-09-28 15:51:15 -0600

    added event listener on button

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 var h1Tag = document.querySelector("#mainH1");
 var para = document.querySelector("#mainPara")
 var startButton = document.querySelector("#startBtn")
+
+function startQuiz(e){
+    
+}
+
+function handleClick(e){
+    e.preventDefault();
+    
+    console.log("click");
+}
+
+startButton.addEventListener("click", handleClick); 
+
+
+startQuiz();
