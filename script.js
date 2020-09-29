0b37ee9294c42b77a90ab58d82d13c05120a6811
jaredcito <jared.seefried@yahoo.com>
2020-09-28 18:21:08 -0600

    working through homework trying to make the timer function

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 var hTag = document.querySelector("#mainH");
 var para = document.querySelector("#mainPara");
 var startButton = document.querySelector("#startBtn");
+var timeRemain = document.querySelector("#timer");
 
-function startQuiz(){
+var timeLeft = 10;
+
+function startTime(){
     
+    var myTimer = setInterval(function() {
+        timeRemain--;
+            if(timeLeft === 0) {
+            clearInterval(myTimer);  
+            }
+        }, 1000)
+    timeRemain.textContent = timeLeft + " seconds remaining.";
+    startQuiz();
 }
 
+function startQuiz(){
+    console.log("question #1")
+  }
+  
+    
 function handleClick(e){
     e.preventDefault();
+    startTime();
     if( e.target.matches("button") ){
     hTag.style.display = "none";
     para.style.display = "none";
     startButton.style.display = "none";
-    }
+    } 
+    
 }
 
 startButton.addEventListener("click", handleClick); 
 
 
-startQuiz();
